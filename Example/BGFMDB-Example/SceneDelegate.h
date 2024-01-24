a5d8100b76ed41e25b3701a6ec06a32a19e81095
yuemoj <yj_745@163.com>
2024-01-24 15:57:10 +0800

    # feat - 对原有库的改造完成, 制作成私有spec, 原库对fmdb的使用是直接使用源码文件, 不能和fmdb同步更新, 现更改为依赖fmdb

---
+++ Example/BGFMDB-Example/SceneDelegate.h
@@ -0,0 +1,15 @@
+//
+//  SceneDelegate.h
+//  BGFMDB-Example
+//
+//  Created by HYT200841559 on 2024/1/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
